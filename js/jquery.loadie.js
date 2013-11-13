6ddbaf30644e90c63d1d60fdc9ef20422d9aefe7
Dominik Schmidt <visualcake@me.com>
2013-11-13 10:59:48 +0100

    Updated loadie.js script

--- js/jquery.loadie.js
+++ js/jquery.loadie.js
@@ -1,26 +1,43 @@
 (function( $ ) {
-  function uniqueId() {
+  var Loadie = {};
+
+  /*
+   * Generate a unique id for more than one loadie
+   */
+  Loadie.uid = function() {
     var newDate = new Date;
     return newDate.getTime();
-  }
+  };
 
-  function destroyGUI(dom) {
+  /*
+   * Finishes and fades the loadie out.
+   */
+  Loadie.finish = function(dom) {
     var loadie = $('#loadie-' + dom.data('loadie-id'), dom);
     loadie.fadeOut(200);
   }
 
-  function updateGUI(dom, percent) {
+  /*
+   * Updates loadie with a float
+   *
+   * Loadie.update(0.2)
+   * Loadie.update(1) // Finishes loadie, too
+   */
+  Loadie.update = function(dom, percent) {
     var loadie = $('#loadie-' + dom.data('loadie-id'), dom);
     var parentWidth = dom.width();
     loadie.css('width', Math.floor(percent * parentWidth) + "px");
   }
 
-  function createGUI(dom, percent) {
-    var uid = uniqueId();
+  /*
+   * Loadie.js initializer
+   */
+  Loadie.init = function(dom, percent) {
+    var uid = this.uid();
     var loadie = dom.append($('<div id="loadie-' + uid + '" class="loadie"></div>'));
     dom.data('loadie-id', uid);
     dom.css('position', 'relative');
-    updateGUI(dom, percent);
+    this.update(dom, percent);
   }
 
   $.fn.loadie = function(percent, callback) {
@@ -28,14 +45,14 @@
     var parent = $(this);
 
     if(parent.data('loadie-loaded') !== 1) {
-      createGUI(parent, percent);
+      Loadie.init(parent, percent);
     } else {
-      updateGUI(parent, percent);
+      Loadie.update(parent, percent);
     }
     if(percent >= 1) {
       setTimeout(function() {
-        destroyGUI(parent);
-      }, 700);
+        Loadie.finish(parent);
+      }, 200);
     }
     parent.data('loadie-loaded', 1);
     return this;
